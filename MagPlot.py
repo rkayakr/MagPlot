1a1dde009070b90d2077260973bafdb2ae8a7ff2
KD8CGH <40500870+rkayakr@users.noreply.github.com>
2021-10-02 21:31:07 -0400

    Add files via upload

---
+++ MagPlot.py
@@ -0,0 +1,331 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+plots PSWS magnetometer data in runmag.log
+
+windows version hardcoded homepath directory location
+for Pi comment out windows homepath and uncomment Pi  lines
+
+expects filemane to process in files.txt in PSWS/magnetometer
+expects magnetometer data files in PSWS/magnetometer/Mdata
+leaves plots in PSWS/magnetometer/Mplot
+
+uses modified WWV_utility2.py
+Bob Benedict, KD8CGH, 10/02/2021
+
+create text file "files.txt" in homepath directory
+  filename 
+
+  ...
+
+loads file name
+plots absolute, relative, total (if available) and temperature plots
+magnetometer results are filtered before plotting
+
+uses modified WWV_utility2.py
+20 February 2020
+WWV utility file
+Routines and classes used in WWV file management and graphing
+David Kazdan, AD8Y
+John Gibbons, N8OBJ - mods to plot header 2/3/20
+
+"""
+
+#import os # uncomment for pi
+from os import path
+import sys
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib import ticker
+from scipy.signal import filtfilt, butter
+import datetime  
+from Mag_utility import time_string_to_decimals
+
+'''  #uncomment for Pi
+# ~ points to users home directory - usually /home/pi/
+homepath = os.path.expanduser('~')
+
+# imbed the trailing / in the home path
+homepath = homepath + "/magnetometer/"
+
+#comment out windows homepath
+'''
+
+homepath = "E:\\Documents\\PSWS\\magnetometer\\"  # set your windows path, comment out for Pi
+
+names = open(homepath+"files.txt","r")
+
+Filenames=['a' for a in range (10)]
+Filedates=['a' for a in range (10)]
+PrFilenames=['a' for a in range (10)]
+Callsigns=['a' for a in range (10)]
+
+nfiles = 0
+
+colors=['b','g','r','c','m','y','tab:orange','tab:gray','tab:purple','tab:brown']
+
+while True:
+    temp = names.readline()
+    if  len(temp) == 0:
+        break
+    print(temp+'\n')
+    Filenames[nfiles]=temp.strip("\n")
+    fdate = Filenames[nfiles].find("/") # find start of filename after subdirectory
+    if fdate==-1 :  # if / not found try \
+        fdate = Filenames[nfiles].find("\\")    
+    
+    Filedates[nfiles]=temp[17:23]
+    cstart = Filenames[nfiles].find("-")
+    Callsigns[nfiles]=temp[fdate+1:cstart]
+    Filedates[nfiles]=temp[cstart+1:cstart+9]
+    print(Callsigns[nfiles])
+    print(Filedates[nfiles])    
+    nfiles=nfiles + 1
+        
+print('number of files',nfiles)
+if nfiles > 1 :
+    print('1 file limit this version')
+    sys.exit(0)
+
+DATADIR = homepath + 'magdata/'
+
+#saved plot directrory
+PlotDir = homepath + 'Mplot/'
+
+'''
+read first file
+'''
+PrFilenames=(DATADIR + Filenames[0])
+
+if (path.exists(PrFilenames)):
+    print('File ' + PrFilenames + ' found!\nProcessing...')
+else:
+    print('File ' + PrFilenames + ' not available.\nExiting disappointed...')
+    sys.exit(0)
+
+with open(PrFilenames, 'r') as dataFile:
+    dataReader=csv.reader(dataFile)
+    data = list(dataReader)
+    Header = data.pop(0)
+
+print('Ready to start processing records')
+
+# Prepare data arrays for multifile plotting
+hours=[[],[],[],[],[],[],[],[],[],[]]
+rtemp=[[],[],[],[],[],[],[],[],[],[]]
+ltemp=[[],[],[],[],[],[],[],[],[],[]]
+x=[[],[],[],[],[],[],[],[],[],[]] # will be second data set, received power 9I20
+y=[[],[],[],[],[],[],[],[],[],[]]
+z=[[],[],[],[],[],[],[],[],[],[]]
+rx=[[],[],[],[],[],[],[],[],[],[]]
+ry=[[],[],[],[],[],[],[],[],[],[]]
+rz=[[],[],[],[],[],[],[],[],[],[]]
+total=[[],[],[],[],[],[],[],[],[],[]]
+
+filtx=[[],[],[],[],[],[],[],[],[],[]]
+filty=[[],[],[],[],[],[],[],[],[],[]]
+filtz=[[],[],[],[],[],[],[],[],[],[]]
+filtrx=[[],[],[],[],[],[],[],[],[],[]]
+filtry=[[],[],[],[],[],[],[],[],[],[]]
+filtrz=[[],[],[],[],[],[],[],[],[],[]]
+filttot=[[],[],[],[],[],[],[],[],[],[]]
+
+LateHour=False # flag for loop going past 23:00 hours
+
+# eliminate all metadata saved at start of file - Look for UTC (CSV headers)
+#find first row of data0
+FindUTC = 0
+recordcnt  = 0
+freqcalc = 0
+calccnt = 0
+plot=0
+istotal = True
+
+for row in data:
+    decHours=time_string_to_decimals(row[0])
+    if decHours > 23:
+       LateHour=True # went past 23:00 hours    
+    if (not LateHour) or (LateHour and (decHours>23)): # Otherwise past 23:59:59.  Omit time past midnight.
+        hours[0].append(decHours) # already in float because of conversion to decimal hours.
+        rtemp[0].append(float(row[1]))
+        ltemp[0].append(float(row[2]))
+        x[0].append(float(row[3])) 
+        y[0].append(float(row[4]))
+        z[0].append(float(row[5]))
+        rx[0].append(float(row[6]))
+        ry[0].append(float(row[7]))
+        rz[0].append(float(row[8]))
+        if len(row) > 9 :
+            total[0].append(float(row[9]))
+        else: 
+            print(' no tot\n')
+            istotal = False
+            
+print('nf ',0,'len hours',len(hours[0]))
+
+###############################################################################################
+# Find max and min 
+min_T=min(np.amin(ltemp[0]),np.amin(rtemp[0]))
+max_T=max(np.amax(ltemp[0]),np.amax(rtemp[0]))
+min_abs=min(np.amin(x[0]),np.amin(y[0]),np.amin(z[0]))
+max_abs=max(np.amax(x[0]),np.amax(y[0]),np.amax(z[0]))
+print('min T',min_T,'max T',max_T,'min abs ',min_abs,'max abs ',max_abs)
+min_rel=min(np.amin(rx[0]),np.amin(ry[0]),np.amin(rz[0]))
+max_rel=max(np.amax(rx[0]),np.amax(ry[0]),np.amax(rz[0]))
+min_tot=np.amin(total[0])
+max_tot=np.amax(total[0])
+print('min rel ',min_rel,'max rel ',max_rel)
+
+#%% Create an order 3 lowpass butterworth filter.
+# This is a digital filter (analog=False)
+# Filtering at .01 to .004 times the Nyquist rate seems "about right."
+# The filtering argument (Wn, the second argument to butter()) of.01
+# represents filtering at .05 Hz, or 20 second weighted averaging.
+# That corresponds with the 20 second symmetric averaging window used in the 1 October 2019
+# Excel spreadsheet for the Festival of Frequency Measurement data.
+#FILTERBREAK=.005 #filter breakpoint in Nyquist rates. N. rate here is 1/sec, so this is in Hz.
+FILTERBREAK=0.005 #filter breakpoint in Nyquist rates. N. rate here is 1/sec, so this is in Hz.
+FILTERORDER=6
+b, a = butter(FILTERORDER, FILTERBREAK, analog=False, btype='low')
+
+filtrx[0] = filtfilt(b, a, rx[0])
+filtry[0] = filtfilt(b, a, ry[0])
+filtrz[0] = filtfilt(b, a, rz[0])
+
+filtx[0] = filtfilt(b, a, x[0]) 
+filty[0] = filtfilt(b, a, y[0])
+filtz[0] = filtfilt(b, a, z[0])
+
+filttot[0] = filtfilt(b, a, total[0])
+
+# start plots
+
+M = 7  # number of plot y ticks
+lw=0.6  # plot line width
+pdpi=250 # final plot image dpi
+
+'''
+============================ relative data plot
+'''
+
+
+fig = plt.figure(figsize=(12,8))
+plt.rcParams['font.size'] = '7'
+gs = fig.add_gridspec(3, hspace=0)
+axs = gs.subplots(sharex=True)
+fig.suptitle(Callsigns[0] + ' Rel Mag Data Plot '+Filedates[0])
+
+axs[2].set_xlabel('UTC Hour')
+axs[0].plot(hours[0], filtrx[0], colors[0], label='rx',linewidth=lw)
+axs[0].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[0].yaxis.set_major_locator(yticks)
+axs[0].grid(axis='both')
+
+axs[1].plot(hours[0], filtry[0], colors[1], label='ry',linewidth=lw)
+axs[1].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[1].yaxis.set_major_locator(yticks)
+axs[1].grid(axis='both')
+
+axs[2].grid(axis='both')
+axs[2].plot(hours[0], filtrz[0], colors[2], label='rz',linewidth=lw)
+axs[2].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[2].yaxis.set_major_locator(yticks)
+axs[2].grid(axis='both')  # must set before and after !!!
+
+axs[2].set_xlabel('UTC Hour')
+axs[2].set_xlim(0,24) # UTC day
+axs[2].set_xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], minor=False)
+
+for ax in axs:
+    ax.label_outer()
+plt.grid(axis='both')
+GraphFile = Filedates[0]+'_' + Callsigns[0] + 'r.png'
+PlotGraphFile = PlotDir + GraphFile
+plt.savefig(PlotDir + GraphFile, dpi=pdpi, orientation='landscape')
+print('Plot File: ' + GraphFile + '\n')
+
+'''absolute data plot
+'''
+
+fig = plt.figure(figsize=(12,8))
+gs = fig.add_gridspec(3, hspace=0)
+axs = gs.subplots(sharex=True)
+fig.suptitle(Callsigns[0] + ' Abs Mag Data Plot '+Filedates[0])
+axs[0].plot(hours[0], filtx[0], colors[0], label='x',linewidth=lw)
+axs[0].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[0].yaxis.set_major_locator(yticks)
+axs[0].grid(axis='both')
+
+axs[1].plot(hours[0], filty[0], colors[1], label='y',linewidth=lw)
+axs[1].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[1].yaxis.set_major_locator(yticks)
+axs[1].grid(axis='both')
+
+axs[2].grid(axis='both')
+axs[2].plot(hours[0], filtz[0], colors[2], label='z',linewidth=lw)
+axs[2].legend(loc="lower right",  frameon=False)
+yticks = ticker.MaxNLocator(M)
+axs[2].yaxis.set_major_locator(yticks)
+axs[2].grid(axis='both')  # must set before and after !!!
+
+axs[2].set_xlabel('UTC Hour')
+axs[2].set_xlim(0,24) # UTC day
+axs[2].set_xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], minor=False)
+
+for ax in axs:
+    ax.label_outer()
+plt.grid(axis='both')
+GraphFile = Filedates[0]+'_' + Callsigns[0] + 'a.png'
+PlotGraphFile = PlotDir + GraphFile
+plt.savefig(PlotDir + GraphFile, dpi=pdpi, orientation='landscape')
+print('Plot File: ' + GraphFile + '\n')
+
+
+'''total data plot
+'''
+if istotal==True:
+    plt.rcParams['font.size'] = '10'
+    fig = plt.figure(figsize=(12,8)) # inches x, y with 72 dots per inch
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('UTC Hour')
+    ax.set_xlim(0,24) # UTC day
+    ax.set_xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], minor=False)
+    ax.plot(hours[0], filttot[0], colors[0],label='total') # color k for black
+    plt.grid(axis='both')
+    plt.title(Callsigns[0] + ' Mag Total Data Plot '+Filedates[0])
+    ax.legend(loc="lower right",  frameon=False)
+
+    GraphFile = Filedates[0]+'_' + Callsigns[0] + 't.png'
+    PlotGraphFile = PlotDir + GraphFile
+    plt.savefig(PlotDir + GraphFile, dpi=pdpi, orientation='landscape')
+    print('Plot File: ' + GraphFile + '\n')
+
+
+'''temperature data plot
+'''
+plt.rcParams['font.size'] = '10'
+fig = plt.figure(figsize=(12,8)) # inches x, y with 72 dots per inch
+ax = fig.add_subplot(111)
+ax.set_xlabel('UTC Hour')
+ax.set_xlim(0,24) # UTC day
+ax.set_xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], minor=False)
+ax.plot(hours[0], rtemp[0], colors[0],label='Remote T') # color k for black
+ax.plot(hours[0], ltemp[0], colors[1],label='Local T') 
+ax.legend(loc="lower right",  frameon=False)
+plt.grid(axis='both')
+plt.title(Callsigns[0] + ' Temperature Data Plot '+Filedates[0])
+
+GraphFile = Filedates[0]+'_' + Callsigns[0] + 'rt.png'
+PlotGraphFile = PlotDir + GraphFile
+plt.savefig(PlotDir + GraphFile, dpi=pdpi, orientation='landscape')
+print('Plot File: ' + GraphFile + '\n')
+
+#-------------------------------------------------------------------
+print('Exiting python multi plot program gracefully')
---
+++ Mag_utility.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+10/2/2021 mod by Bob Benedict KD8CGH for mag plot
+WWV utility file
+Routines and classes used in WWV file management and graphing
+David Kazdan, AD8Y
+John Gibbons, N8OBJ - mods to plot header 2/3/20
+
+"""
+
+#%% utility function needed here to convert ISO time into decimal hours
+def time_string_to_decimals(time_string): #returns float decimal hours
+    
+    #print('Input time string=',time_string)
+#    if (NewHdr = 'New'):   # if new header strip off date and Zulu stuff
+#        time_string = time_string[11:-1]  # Hack off date 'YYYY-MM-DDT' and ending 'Z'
+    time_string = time_string[12:]  # Hack off date 'YYYY-MM-DDT' and ending 'Z'
+#    print('Used Time_String=',time_string)
+    fields=time_string.split(":")
+    hours=float(fields[0]) if len(fields)>0 else 0.0
+    minutes=float(fields[1])/60. if len(fields)>0 else 0.0
+    seconds=float(fields[2])/3600. # if len(fields)>0 else 0.0
+#    print('Hr=',hours, '  Min=',minutes, '  Sec=',seconds, '  ',float(fields[2]),'\n')
+    return (hours + minutes + seconds)
+
